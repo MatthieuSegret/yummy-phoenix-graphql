58d244332771e0cc794fd8bbf60ce0606d3959d9
Matthieu Segret <matthieu.segret@gmail.com>
2018-01-25 10:52:48 +0100

    Create comment integration tests

---
+++ test/integrations/create_comment_test.exs
@@ -0,0 +1,22 @@
+defmodule YummyWeb.Integrations.CreateCommentTest do
+  use YummyWeb.IntegrationCase, async: true
+
+  setup do
+    user = insert(:user)
+    {:ok, %{user: user}}
+  end
+
+  test "create comment on recipe page", %{session: session, user: user} do
+    recipe = insert(:recipe)
+
+    session
+    |> user_sign_in(user: user)
+
+    |> visit("/recipes/#{recipe.id}")
+    |> fill_in(text_field("Nouveau commentaire"), with: "A new comment")
+    |> click(button("Commenter"))
+
+    |> assert_has(css(".comment .comment-content", text: "A new comment"))
+    |> assert_has(css(".comment .comment-author em", text: user.name))
+  end
+end
