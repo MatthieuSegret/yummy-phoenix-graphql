3e65da3574c76d69670609cb2f5e655302116532
Matthieu Segret <matthieu.segret@gmail.com>
2018-05-18 16:26:43 +0200

    Remove data updates from migrations. Use raw SQL in futur

--- priv/repo/migrations/20171109180910_add_uploads_to_recipes.exs
+++ priv/repo/migrations/20171109180910_add_uploads_to_recipes.exs
@@ -1,20 +1,10 @@
 defmodule Yummy.Repo.Migrations.AddUploadsToRecipes do
   use Ecto.Migration
-  alias Yummy.Repo
-  alias Yummy.Recipes.Recipe
-  alias Ecto.{Changeset, UUID}
 
   def up do
     alter table(:recipes) do
       add :image_url, :string
       add :uuid, :string
-    end
-
-    flush()
-    for recipe <- Repo.all(Recipe) do
-      recipe
-      |> Changeset.change(uuid: UUID.generate())
-      |> Repo.update()
     end
   end
 
